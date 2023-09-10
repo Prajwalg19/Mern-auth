4f367fb31fe2e7de8fd6f8f03dbb6d2b2592128a
Prajwalg19 <theimerpical8@gmail.com>
2023-09-10 12:10:17 +0530

    error handling

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,19 +1,21 @@
 import { model } from "../models/usermodel.js";
 import bcryptjs from "bcryptjs"; //for encrypting the user password to a hash value for security & privacy reasons
-export const auth = async (req, res) => {
+import { handleError } from "../utils/errors.js";
+export const auth = async (req, res, next) => {
     try {
         let { userName, email, pass } = req.body;
         const hashedPass = bcryptjs.hashSync(pass, 12);
         const newUser = new model({
+            //creation of the document using the model created using the schema defined
             userName, //after es6 , if the key and variable name of the value are of the same name ,then the shorthand of writing only one was introduced
             email,
             pass: hashedPass,
         });
-        await newUser.save();
+        await newUser.save(); //saves the document inside of the collection.
         const promise = res.status(201).json("User created Successfully");
         console.log(promise);
     } catch (e) {
-        res.status(500).json(e.message);
+        next(handleError(300, "something went wrong"));
     }
 };
 
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import "dotenv/config"; //gives access to the environment variables defined in the .env file
 import userRoutes from "./api/routes/userRoutes.js";
 import authRoutes from "./api/routes/authRoutes.js";
+import { handleError } from "./api/utils/errors.js";
 const app = express();
 const db = mongoose
     .connect(process.env.URL) // process.env is a object with the members that are the key value pairs of the details written inside the .env file
@@ -18,3 +19,9 @@ app.use(express.json()); //converts the recieved request body json format into j
 app.use("/api/user", userRoutes); // the use is a method that takes a middleware (functions that have the request and response object , used in between the requests and responses, the /api url has all the routes under the userRoutes module)
 
 app.use("/api/auth", authRoutes); //url for authentication
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode ?? 500;
+    const message = err.message || "Internal server error";
+    res.status(statusCode).json({ success: false, message, statusCode });
+});
